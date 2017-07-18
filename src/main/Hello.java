32bbbe4079bda864165a03742ff5489f83ca5738
grawix <grawix@gmail.com>
2017-07-18 07:34:15 +0200

    Dodane Hello w Selenium z Mavenem

---
+++ src/main/Hello.java
@@ -0,0 +1,24 @@
+package main;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hello {
+    public static WebDriver driver;
+    public static void main(String[] args){
+
+        System.out.println("starting selenium web driver");
+        System.setProperty("webdriver.chrome.driver", "./target/classes/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+        try{
+            Thread.sleep(2000);
+        }
+        catch(Exception e){}
+
+        driver.close();
+        driver.quit();
+    }
+
+}
