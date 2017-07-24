53fa1c754dcff8644cc89394b689de38432318b9
grawix <grawix@gmail.com>
2017-07-24 20:52:52 +0200

    Conflict

---
+++ src/main/Conflicts.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Conflicts {
+    public static void conflict1() {
+        System.out.println("Line 1");
+        System.out.println("Lorem ipsum dolor sit amet");
+        System.out.println("Quisque faucibus felis nec nunc malesuada porta nec ut sem");
+        System.out.println("Nullam sed lectus sit amet augue posuere finibus");
+        System.out.println("Interdum et malesuada fames ac ante ipsum primis in faucibus");
+        System.out.println("Ut ultrices tristique imperdiet");
+        System.out.println("Nullam in orci at nibh vestibulum tempus");
+    }
+
+    public static void conflict2() {
+        System.out.println("Line 2");
+        System.out.println("Lorem ipsum dolor sit amet");
+        System.out.println("Quisque faucibus felis nec nunc malesuada porta nec ut sem");
+        System.out.println("Nullam sed lectus sit amet augue posuere finibus");
+        System.out.println("Interdum et malesuada fames ac ante ipsum primis in faucibus");
+        System.out.println("Ut ultrices tristique imperdiet");
+        System.out.println("Nullam in orci at nibh vestibulum tempus");
+    }
+
+}
