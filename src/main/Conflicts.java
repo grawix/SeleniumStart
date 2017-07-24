57c78714ba01b4bfec02502ec969df5125bd964a
grawix <grawix@gmail.com>
2017-07-24 20:56:53 +0200

    Conflict 2

--- src/main/Conflicts.java
+++ src/main/Conflicts.java
@@ -3,7 +3,7 @@ package main;
 public class Conflicts {
     public static void conflict1() {
         System.out.println("Line 1");
-        System.out.println("Lorem ipsum dolor sit amet");
+        System.out.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit");
         System.out.println("Quisque faucibus felis nec nunc malesuada porta nec ut sem");
         System.out.println("Nullam sed lectus sit amet augue posuere finibus");
         System.out.println("Interdum et malesuada fames ac ante ipsum primis in faucibus");
@@ -11,9 +11,13 @@ public class Conflicts {
         System.out.println("Nullam in orci at nibh vestibulum tempus");
     }
 
+    public static void newConflict(){
+        System.out.println("Line 3");
+    }
+
     public static void conflict2() {
-        System.out.println("Line 2");
-        System.out.println("Lorem ipsum dolor sit amet");
+        System.out.println("Line 3");
+        System.out.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit");
         System.out.println("Quisque faucibus felis nec nunc malesuada porta nec ut sem");
         System.out.println("Nullam sed lectus sit amet augue posuere finibus");
         System.out.println("Interdum et malesuada fames ac ante ipsum primis in faucibus");
