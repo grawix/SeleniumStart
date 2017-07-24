98079a38c2f229f6640475c03539b98957391520
grawix <grawix@gmail.com>
2017-07-24 21:02:45 +0200

    Tworzenie konfliktu

--- src/main/Conflicts.java
+++ src/main/Conflicts.java
@@ -3,6 +3,7 @@ package main;
 public class Conflicts {
     public static void conflict1() {
         System.out.println("Line 1");
+        System.out.println();
         System.out.println("Lorem ipsum dolor sit amet");
         System.out.println("Quisque faucibus felis nec nunc malesuada porta nec ut sem");
         System.out.println("Nullam sed lectus sit amet augue posuere finibus");
@@ -11,8 +12,16 @@ public class Conflicts {
         System.out.println("Nullam in orci at nibh vestibulum tempus");
     }
 
+    public static void conflict() {
+        for(int i=0;i<10;i++){
+            System.out.println("Line " + i);
+            System.out.println("Lorem ipsum dolor sit amet");
+        }
+    }
+
     public static void conflict2() {
         System.out.println("Line 2");
+        System.out.println();
         System.out.println("Lorem ipsum dolor sit amet");
         System.out.println("Quisque faucibus felis nec nunc malesuada porta nec ut sem");
         System.out.println("Nullam sed lectus sit amet augue posuere finibus");
